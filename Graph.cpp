0ff4fcac3f248c85ba3a29d53a1c081e7f87e9d5
Uri Kialy <141826647+UriKialy@users.noreply.github.com>
2024-09-13 15:11:45 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -5,6 +5,9 @@ Graph::Graph(vector<vector<int>> adjMat): adjMat(adjMat) {
     numEdges = 0;
     for (int i = 0; i < numVertices; i++) {
         for(int j=0;j<numVertices;j++){
+            if(adjMat.at(i).at(i)!=0){
+                throw invalid_argument("Diagonal elements must be zero");
+            }
             if(adjMat.at(i).at(j)!=0){
             numEdges++;
         }
@@ -19,4 +22,10 @@ int Graph::getNumVertices(){
 }
 vector<vector<int>> Graph::getAdjMat(){
     return adjMat;
+}
+void Graph::addEdge(int from, int to, int weight){
+    if (from < 0 || from >= numVertices || to < 0 || to >= numVertices||from==to) {
+        throw invalid_argument("Invalid vertex index");
+    }
+    adjMat.at(from).at(to) = weight;
 }
